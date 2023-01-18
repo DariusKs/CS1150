42f7ddaf69ad6a862f8c2d9d813c02cd00addc82
DariusKs <114531697+DariusKs@users.noreply.github.com>
2023-01-18 14:43:43 -0700

    Add files via upload

---
+++ cs1150-cw01/SmithDariusLO1.java
@@ -0,0 +1,10 @@
+
+public class SmithDariusLO1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello Java")
+		//System.out.print("sdafasdf")
+	}
+
+}
